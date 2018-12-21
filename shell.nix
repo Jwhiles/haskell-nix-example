9ae2af4f9acfd7b581d77cc62fd0a5c26d5dcbc0
John Whiles <jwhiles@Johns-Air-2.lan>
2018-12-21 14:52:29 +0000

    Add a servant server that works :o

--- shell.nix
+++ shell.nix
@@ -6,11 +6,15 @@ let
       ${pkgs.haskellPackages.hpack}/bin/hpack && 
       ${pkgs.cabal2nix}/bin/cabal2nix . > hask.nix
     '';
+
+  getHaskelly
+    = pkgs.writeShellScriptBin "hask" "nix-shell --attr env project.nix";
 in
   stdenv.mkDerivation {
     name = "shellac";
     
     buildInputs = [
       buildCabal
+      getHaskelly
     ];
   }
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,50 @@
+{-# Language DataKinds #-}
+{-# Language DeriveAnyClass #-}
+{-# Language DeriveGeneric #-}
+{-# Language TypeOperators #-}
+{-# Language OverloadedStrings #-}
+
+
 module Main where
 
+import Data.Text
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Time (UTCTime)
+import GHC.Generics (Generic)
+import Servant
+import Servant.Server
+import Network.Wai (Application)
+
+import qualified Network.Wai.Handler.Warp
+
+data Cutie = Amelie | Waluigi
+  deriving (Generic, ToJSON)
+
+
+-- /cutie/{cutieId}
+-- /allCuties
+type CutieFinder = 
+  "cutie" :> Capture "cutieId" Integer :> Get '[JSON] Cutie
+  :<|> 
+  "allCuties" :> Get '[JSON] [Cutie]
+
+server :: Server CutieFinder
+server = 
+  handleGetCutie :<|>
+  handleGetAllCuties
+
+
+handleGetCutie :: Integer -> Handler Cutie
+handleGetCutie cutieId = 
+  case cutieId of 1 -> return Amelie
+                  2 -> return Waluigi
+                  _ -> fail "not a cutie"
+
+handleGetAllCuties :: Handler [Cutie]
+handleGetAllCuties = return [Amelie, Waluigi]
+
 main :: IO ()
-main = putStrLn "hello"
+main = Network.Wai.Handler.Warp.run 8080 application
+
+application :: Application
+application = serve (Proxy :: Proxy CutieFinder) server
